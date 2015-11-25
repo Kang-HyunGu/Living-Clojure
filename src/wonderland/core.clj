44d782f570e58155f6d9ec968ce6457f1647f393
강현구 <hkkang@woowahan.com>
2015-11-25 21:07:26 +0900

    Baby Steps with simple Values

--- src/wonderland/core.clj
+++ src/wonderland/core.clj
@@ -4,3 +4,40 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(/ 1 3)
+
+(/ 1 3.0)
+
+"jam"
+
+:jam
+
+\j
+
+true
+
+false
+
+nil
+
+(+ 1 1)
+
+(+ 1 (+ 8 3))
+
+'(1 2 "jam" :marmalade-jar)
+
+'(1, 2, "jam", :bee)
+
+(first '(:rabbit :pocket-watch :marmalade :door))
+
+(rest '(:rabbit :pocket-watch :marmalade :door))
+
+(first (rest '(:rabbit :pocket-watch :marmalade :door)))
+
+(first (rest (rest '(:rabbit :pocket-watch :marmalade :door))))
+
+(first (rest (rest (rest '(:rabbit :pocket-watch :marmalade :door)))))
+
+(first (rest (rest (rest (rest '(:rabbit :pocket-watch :marmalade :door))))))
+
