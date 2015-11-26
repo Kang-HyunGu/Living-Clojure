6a1b483a5e121719683db41d67738b005828f0ec
강현구 <hkkang@woowahan.com>
2015-11-26 22:58:21 +0900

    Put Your Clojure Data in Collections Using a List Collection What Can We Do with Lists? Using Vectors for Collecting Data by Index

--- src/wonderland/core.clj
+++ src/wonderland/core.clj
@@ -41,3 +41,30 @@ nil
 
 (first (rest (rest (rest (rest '(:rabbit :pocket-watch :marmalade :door))))))
 
+(cons 5 '())
+
+(cons 5 nil)
+
+(cons 4 (cons 5 nil))
+
+(cons 3 (cons 4 (cons 5 nil)))
+
+(cons 2 (cons 3 (cons 4 (cons 5 nil))))
+
+'(1 2 3 4 5)
+
+(list 1 2 3 4 5)
+
+[:jar1 1 2 3 :jar2]
+
+(first [:jar1 1 2 3 :jar2])
+
+(rest [:jar1 1 2 3 :jar2])
+
+(nth [:jar1 1 2 3 :jar2] 0)
+
+(nth [:jar1 1 2 3 :jar2] 2)
+
+(last [:rabbit :pocket-watch :marmalade])
+
+(last '(:rabbit :pocket-watch :marmalade))
